ff87f81a6567f3070193769d2f486ae30f33f37a
Arjun <razar87@gmail.com>
2013-10-13 20:14:10 -0400

    Initial commit

---
+++ vendor/bundle/gems/ffi-1.9.0/ext/ffi_c/siteconf20131013-74142-1cgc5r5.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131013-74142-1v4mc1g"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
