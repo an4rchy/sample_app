9dba4937a0d37b14ed3e75c927ebb8e927f2a4d3
Arjun <razar87@gmail.com>
2013-10-18 00:25:41 -0400

    ch4

--- Gemfile
+++ Gemfile
@@ -7,12 +7,16 @@ gem 'rails', '4.0.0'
 group :development, :test, :production do
   gem 'pg', '0.15.1'
   gem 'rspec-rails', '2.13.1'
-  gem 'rails_12factor', '0.0.2'
+  gem 'guard-rspec', '2.5.0'
+  gem 'spork-rails', '4.0.0'
+  gem 'guard-spork', '1.5.0'
+  gem 'childprocess','0.3.6'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+  gem 'libnotify', '0.8.0'
 end
 
 gem 'sass-rails', '4.0.0'
@@ -27,6 +31,5 @@ group :doc do
 end
 
 group :production do
-  gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,20 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  let(:base_title) {"SampleApp |"}
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-
-    it "should have the right title" do
+    it "should have the base title" do 
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} Home")
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App')
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
     end
   end
   describe "Help page" do
@@ -22,7 +25,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -33,7 +36,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} About")
+      expect(page).to have_title("#{base_title} | About")
     end
   end
   describe "Contact page" do
@@ -43,7 +46,7 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
